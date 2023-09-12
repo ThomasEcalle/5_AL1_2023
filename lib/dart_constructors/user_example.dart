a65d91c7a1a5f77351ffd6138292b9bc393f92f3
Thomas Ecalle <thomasecalle@gmail.com>
2023-09-12 15:04:59 +0200

    Add centered text

--- lib/dart_constructors/user_example.dart
+++ lib/dart_constructors/user_example.dart
@@ -1,13 +1,27 @@
 class User {
+  final int age;
   final String firstName;
   final String lastName;
 
-  const User({required this.firstName, required this.lastName});
+  const User(
+    this.age, {
+    required this.firstName,
+    required this.lastName,
+  });
 }
 
 void main() {
-  const user = User(firstName: 'toto', lastName: 'machin');
-  final user2 = User(firstName: 'toto', lastName: 'machin');
+  const user = User(
+    42,
+    firstName: 'toto',
+    lastName: 'machin',
+  );
+
+  const user2 = User(
+    42,
+    firstName: 'toto',
+    lastName: 'machin',
+  );
 
   print(user == user2);
 }
