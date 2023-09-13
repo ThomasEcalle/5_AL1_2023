50619687443d74c0d0300c41f17ef76bb2a4395e
Thomas Ecalle <thomasecalle@gmail.com>
2023-09-13 10:21:47 +0200

    Add second button exercise

--- lib/home_tabs/calendar_tab.dart
+++ lib/home_tabs/calendar_tab.dart
@@ -1,13 +1,33 @@
-import 'package:al_2023/home_tabs/button_square.dart';
 import 'package:flutter/material.dart';
 
-class CalendarTab extends StatelessWidget {
+import 'button_square_2.dart';
+
+class CalendarTab extends StatefulWidget {
   const CalendarTab({super.key});
 
   @override
+  State<CalendarTab> createState() => _CalendarTabState();
+}
+
+class _CalendarTabState extends State<CalendarTab> {
+  Color _color = Colors.white;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: ButtonSquare(),
+    return Container(
+      color: _color,
+      child: Center(
+        child: SquareButton2(
+          onTap: _onButtonTap,
+        ),
+      ),
     );
+  }
+
+  void _onButtonTap() {
+    print('Hello from calendar');
+    setState(() {
+      _color = _color == Colors.white ? Colors.green : Colors.white;
+    });
   }
 }
