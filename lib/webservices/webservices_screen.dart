b2120a55d11b236a93351e6e0307fe8da91b5f31
Thomas Ecalle <thomasecalle@gmail.com>
2023-09-15 10:20:49 +0200

    Add setstate handling on webservice

--- lib/webservices/webservices_screen.dart
+++ lib/webservices/webservices_screen.dart
@@ -12,6 +12,10 @@ class WebServicesScreen extends StatefulWidget {
 }
 
 class _WebServicesScreenState extends State<WebServicesScreen> {
+  bool _loading = true;
+  List<RemoteUser> _users = [];
+  String? _error;
+
   @override
   void initState() {
     super.initState();
@@ -20,21 +24,68 @@ class _WebServicesScreenState extends State<WebServicesScreen> {
 
   void _fetchUsers() async {
     try {
+      await Future.delayed(const Duration(seconds: 2));
       final http.Response response = await http.get(
         Uri.parse('https://reqres.in/api/users?page=1&per_page=10'),
       );
 
       final jsonBody = json.decode(response.body);
 
-      final dynamicList = jsonBody['data'];
+      final dynamicList = jsonBody['data'] as List;
 
+      final List<RemoteUser> users = dynamicList.map((jsonUser) => RemoteUser.fromJson(jsonUser)).toList();
+
+      _users = users;
     } catch (error) {
       print('Error fetching users');
+      _error = 'Une erreur est survenue';
     }
+
+    setState(() {
+      _loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _buildContent(context),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    if (_loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_error != null) {
+      return Center(
+        child: Text(
+          _error ?? '',
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      );
+    }
+
+    if (_users.isEmpty) {
+      return Center(
+        child: Text(
+          'Oups, tu n\'as pas d\'amis',
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: _users.length,
+      itemBuilder: (context, index) {
+        final user = _users[index];
+        return ListTile(
+          title: Text(user.firstName ?? ''),
+        );
+      },
+    );
   }
 }
